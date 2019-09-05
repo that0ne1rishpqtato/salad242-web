3b419de2a2b9221f5c3dc2c44c5705f7031f0052
Pqtato <29719362+Kore-Development@users.noreply.github.com>
2019-09-05 10:01:56 -0500

    some changes
    contact page and shadow effect

--- src/components/Browsers.vue
+++ src/components/Browsers.vue
@@ -4,7 +4,7 @@
       <span class="title">Browsers</span>
       <li v-for="item in items">
         <a v-bind:href="''+item.url">
-          <img v-bind:src="'https://img.shields.io/badge/-' + item.name + '-' + item.color +'.svg'">
+          <img v-bind:src="'https://img.shields.io/badge/-' + item.name + '-' + item.color +'.svg'" class="shadow">
         </a>
       </li>
     </ul>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="home">
       <div>
-        <span class="title"><img src="../../public/logo.png"></span>
+        <span class="title"><img src="../../public/logo.png" class="shadow"></span>
         <h3>Please select a category above.</h3>
       </div>
     </div>
