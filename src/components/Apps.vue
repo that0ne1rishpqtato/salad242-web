0cd46ea2521ea8b303545d0cfe7d1fa23033ce20
Pqtato <29719362+Kore-Development@users.noreply.github.com>
2019-09-10 12:07:09 -0500

    Update Apps.vue

--- src/components/Apps.vue
+++ src/components/Apps.vue
@@ -3,8 +3,8 @@
     <ul class="list">
       <span class="title">Apps</span>
       <li v-for="item in items">
-        <a v-bind:href="''+item.url">
-          <img v-bind:src="'https://img.shields.io/badge/-' + item.name + '-' + item.color +'.svg'" class="shadow">
+        <a :href="''+item.url">
+          <img :src="'https://img.shields.io/badge/-' + item.name + '-' + item.color +'.svg'" :class="'shadow'">
         </a>
       </li>
     </ul>
