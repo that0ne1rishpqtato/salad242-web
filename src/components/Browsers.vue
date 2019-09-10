0d72817bdce72bb43c086f306d83f0ccc5d1eff8
ItzKORE <jacobdschenke@gmail.com>
2019-09-10 16:39:19 -0500

    changes

--- src/components/Browsers.vue
+++ src/components/Browsers.vue
@@ -25,53 +25,6 @@ export default {
           url: "#/simplecalculator"
         }
       ]
-      /* items: [
-        {
-          name: "Potato",
-          color: "red",
-          url: "https://chrome.google.com/webstore/detail//eikdpecakdfglplkafngboigefiojocg"
-        },
-        {
-          name: "Avocado",
-          color: "red",
-          url: "https://chrome.google.com/webstore/detail//bgkdgnahbdfddpagnmpkhbgdpepmbjbf"
-        },
-        {
-          name: "Kermit's Meme Browser",
-          color: "red",
-          url: "https://chrome.google.com/webstore/detail//jpieeijbjnjpfclbbbmfnoncedamonga"
-        },
-        {
-          name: "Steve The Monkey",
-          color: "red",
-          url: "https://chrome.google.com/webstore/detail//ldjkakppecnkjfcmbcehbmphmkdbhbkj"
-        },
-        {
-          name: "Earls Browser",
-          color: "red",
-          url: "https://chrome.google.com/webstore/detail//bajbkoongpoeddacnegpakakhemifgmk"
-        },
-        {
-          name: "Fortnite Sans Browser",
-          color: "red",
-          url: "https://chrome.google.com/webstore/detail//klkodfbmmgmfpmlgmfgfkekeilkdcdmh"
-        },
-        {
-          name: "Patrick",
-          color: "red",
-          url: "https://chrome.google.com/webstore/detail//cnhcbpmjemifplbaopldlikncpcifjgf"
-        },
-        {
-          name: "Cherry",
-          color: "red",
-          url: "https://chrome.google.com/webstore/detail//odgpbeipjlaiompgjpfjgpdddoephckk"
-        },
-        {
-          name: "Rainboww",
-          color: "red",
-          url: "https://chrome.google.com/webstore/detail//ckfbmfccloijkilcokojoecmeipgdbmb"
-        }
-      ] */
     };
   }
 };
