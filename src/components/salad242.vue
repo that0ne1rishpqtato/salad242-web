ce42acb733ce9e6fa28df4ebb9783709ec7c47bc
Pqtato <29719362+Kore-Development@users.noreply.github.com>
2019-09-12 14:35:45 -0500

    Update salad242.vue

--- src/components/salad242.vue
+++ src/components/salad242.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="browsers">
     <ul class="list">
-      <span class="title">salad242</span>
+      <span class="title"><b>Browsers</b> from salad242</span>
       <li v-for="item in items">
         <a :href="''+item.url">
           <img :src="'https://img.shields.io/badge/-' + item.name + '-' + item.color +'.svg'" :class="'shadow'">
