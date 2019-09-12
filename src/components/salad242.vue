e0c49334bd5e32bd1cd0e1bd8885df5ea18e6f5c
Pqtato <29719362+Kore-Development@users.noreply.github.com>
2019-09-12 13:50:31 -0500

    Update and rename Discontinued.vue to salad242.vue

--- src/components/salad242.vue
+++ src/components/salad242.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="browsers">
     <ul class="list">
-      <span class="title">Discontinued Browsers</span>
+      <span class="title">salad242</span>
       <li v-for="item in items">
         <a :href="''+item.url">
           <img :src="'https://img.shields.io/badge/-' + item.name + '-' + item.color +'.svg'" :class="'shadow'">
@@ -12,9 +12,9 @@
 </template>
 <script>
 export default {
-  name: "Discontinued Browsers",
+  name: "salad242",
   metaInfo: {
-    title: "Discontinued Browsers"
+    title: "salad242"
   },
   data: function() {
     return {
