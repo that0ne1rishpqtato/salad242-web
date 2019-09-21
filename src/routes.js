b547e8d75d2a72293d2744202070e4fda5417e64
Pqtato <29719362+Kore-Development@users.noreply.github.com>
2019-09-20 19:21:18 -0500

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,9 @@
 import Home from "./components/Home.vue";
 import Browsers from "./components/Browsers.vue";
-import Extensions from "./components/Extensions.vue";
-import Apps from "./components/Apps.vue";
-import About from "./components/About.vue";
-import About2 from "./components/About2.vue";
-import Contact from "./components/Contact.vue";
-import SimpleCalculator from "./components/SimpleCalculator.vue";
-import salad242 from "./components/salad242.vue";
-import That0ne1rishPqtato from "./components/That0ne1rishPqtato.vue";
 
 const routes = [
   { path: "/", component: Home },
-  { path: "/browsers", component: Browsers },
-  { path: "/extensions", component: Extensions },
-  { path: "/apps", component: Apps },
-  { path: "/about", component: About },
-  { path: "/about2", component: About2},
-  { path: "/contact", component: Contact },
-  { path: "/simplecalculator", component: SimpleCalculator },
-  { path: "/salad242", component: salad242 },
-  { path: "/that0ne1rishpqtato", component: That0ne1rishPqtato }
+  { path: "/browsers", component: Browsers }
 ];
 
 export default routes;
