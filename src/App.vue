caa2615cee2737fb0e0243fcaea6cf3edcea6190
Pqtato <29719362+Kore-Development@users.noreply.github.com>
2019-09-20 19:20:52 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -12,15 +12,8 @@
           <router-link to="/browsers">Browsers</router-link>
         </li>
         <li>
-          <router-link to="/extensions">Extensions</router-link>
-        </li>
-        <li>
-          <!-- The user count is from shields.io which gets it from the Chrome Web Store -->
-          <a href="//users.that0ne1rishpqtato.gq">Users</a>
-        </li>
-        <li>
-          <!-- Games -->
-          <a href="//games.that0ne1rishpqtato.gq">Games</a>
+          <!-- https://github.com/Kore-Development/that0ne1rishpqtato-web -->
+          <a href="//www.that0ne1rishpqtato.gq">Games</a>
         </li>
       </ul>
     </nav>
